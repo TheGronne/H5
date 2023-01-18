373ad2ea1524b8f3792f9b60313b655f84a5ccf6
Kasper Grønne <kasperg102@gmail.com>
2023-01-18 09:26:17 +0100

    Redo project

---
+++ Controller/TestProject/src/main.cpp
@@ -0,0 +1,10 @@
+#include <mainHeader.h>
+#include <Arduino.h>
+
+void setup() {
+  
+}
+
+void loop() {
+  Serial.println(Sum(2, 5));
+}
---
+++ Controller/TestProject/src/mainHeader.h
@@ -0,0 +1,3 @@
+int Sum(int one, int two){
+    return one + two;
+}
