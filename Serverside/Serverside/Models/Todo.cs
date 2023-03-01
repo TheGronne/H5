8def95710e388722661832bc718b6fec8a653b38
TheGronne <kasperg102@gmail.com>
2023-03-01 20:16:11 +0100

    Fix project add identity

---
+++ Serverside/Serverside/Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Serverside.Models;
+
+namespace Serverside.Models
+{
+    public class Todo
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public int UserID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
